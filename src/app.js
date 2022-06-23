421286829fdcc97a59b6000a71a23ef5a5f7ef30
Keshia Collins <keshia90.kc@googlemail.com>
2022-06-23 15:43:03 +0100

    Managed to add update actor to change the name of the actor

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = async (yargsObj) => {
         } else if (yargsObj.delete) {
             await deleteMovie({ title: yargsObj.title })
             console.log(await listMovies());
-            //to update actor input --updateOne "(Actor name you want to change)" then the title and the new name
+            //to update actor input --update "(Actor name you want to change)" then the title and the new name
         } else if (yargsObj.update){
             await updateMovie({actor:yargsObj.update},{ title: yargsObj.title, actor: yargsObj.actor });
             console.log(await listMovies());
